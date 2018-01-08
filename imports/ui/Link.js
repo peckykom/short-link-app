7893956da32f242a1ba3c9fc9f03391851960b3f
Michal Kopecký <peckykom@users.noreply.github.com>
2018-01-08 22:40:31 +0100

    link list

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Accounts } from 'meteor/accounts-base';
 
-// Andrew code
-//export default class Link extends React.Component {
-//    render() {
-//        return <p>Link component</p>
-//    }
-//}
+import { Links } from '../api/links';
+import LinksList from './LinksList';
 
-// My content
-//export default Link = () => <div>
-//    <button onClick={() => { this.props.history.push('/'); }}>Logout</button>
-//</div>;
 
 export default class Link extends React.Component {
-    onLogout(){
-    Accounts.logout();
+    onLogout() {
+        Accounts.logout();
     }
+    onSubmit(e) {
+        const url = this.refs.url.value.trim();
+
+        e.preventDefault();
+
+        if(url){
+          Links.insert({url});
+            this.refs.url.value = '';
+        }
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.onLogout.bind(this)}>Logout</button>
+                <LinksList/>
+                <p>Add Link</p>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="text" ref="url" placeholder="URL"/>
+                    <button>Add link</button>
+                </form>
             </div>
         );
     }
---
+++ imports/ui/LinksList.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Tracker } from "meteor/tracker";
+import { Links } from "../api/links";
+
+export default class LinksList extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            links:[]
+        };
+        console.log("props", props);
+        console.log(this.state);
+    };
+    componentDidMount(){
+        console.log("componentDidMount LinksList");
+        this.linksTracker = Tracker.autorun(() => {
+            const links = Links.find().fetch();
+            console.log("links", links);
+            this.setState({ links });
+        });
+    }
+    componentWillUnmount(){
+        console.log("componentWillUnmount LinksList");
+        this.linksTracker.stop();
+    }
+    renderLinkListItem(){
+        return this.state.links.map((link)=>{
+            console.log(this.state);
+           return <p key={link._id}>{link.url}</p>;
+        })
+    }
+    render() {
+        return (
+            <div>
+                <p>Links List</p>
+                <div>
+                    {this.renderLinkListItem()}
+                </div>
+            </div>
+        )
+    }
+}
