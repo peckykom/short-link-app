2a778d6e991918362552c340ffff7af6301ca677
Michal Kopecký <peckykom@users.noreply.github.com>
2018-01-14 15:21:12 +0100

    publications and subscriptions

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,3 +1,15 @@
 import { Mongo } from "meteor/mongo";
+import { Meteor } from 'meteor/meteor';
 
 export const Links = new Mongo.Collection('links');
+
+//console meteor to object
+//require('meteor/meteor').Meteor;
+
+if (Meteor.isServer) {
+    Meteor.publish('links',() =>{
+       return Links.find({url: '2'});
+    });
+}
+
+
--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Meteor } from 'meteor/meteor';
 import { Tracker } from "meteor/tracker";
 import { Links } from "../api/links";
 
@@ -14,6 +15,7 @@ export default class LinksList extends React.Component {
     componentDidMount(){
         console.log("componentDidMount LinksList");
         this.linksTracker = Tracker.autorun(() => {
+            Meteor.subscribe('links');
             const links = Links.find().fetch();
             console.log("links", links);
             this.setState({ links });
